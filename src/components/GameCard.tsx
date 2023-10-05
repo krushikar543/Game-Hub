1341f82908d2c2295f05661e7eaf9245fd03afd6
krushikar543 <krushikar.pennabadi@iiitb.org>
2023-10-05 21:43:40 +0530

    did some code clean

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { Game } from "../hooks/FetchGamesResponse"
-import { Card, CardBody, Image, Heading, HStack } from "@chakra-ui/react";
+import { Card, CardBody, Image, Heading, HStack, Box } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 
@@ -13,13 +13,14 @@ const GameCard = ({game} : Props) => {
         <Card width = '300px' borderRadius='10px' overflow='hidden'>
             <Image src = {game.background_image} />
             <CardBody>
-                <Heading fontSize='2xl'>
-                    {game.name}
-                </Heading>
+                
                 <HStack justifyContent='space-between'>
                     <PlatformIconList platform={game.parent_platforms.map(p => p.platform)}/>
                     <CriticScore score = {game.metacritic}/>
                 </HStack>
+                <Heading fontSize='2xl'>
+                    {game.name}
+                </Heading>
             </CardBody>
         </Card>
     )
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,5 +1,5 @@
 import FetchGenres from "../hooks/FetchGenres"
-import { HStack, Image, List, ListItem, Spinner, Button } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Button, Heading } from "@chakra-ui/react";
 import { Genre } from '../hooks/FetchGenres';
 interface Props{
     selectedGenre : Genre | null;
@@ -8,21 +8,25 @@ interface Props{
 
 
 const GenreList = ({selectedGenre, onSelectGenre} : Props) => {
+    
     const {genres, error, isLoading} = FetchGenres();
     if(isLoading) return <Spinner />;
     if(error) return null;
     return(
+        <>
+        <Heading fontSize='2xl' margin={3}> Genre </Heading>
         <List>
             {genres.map((genre) => (  
             <ListItem key={genre.id}>
                 <HStack >
-                    <Image boxSize='32px' paddingY={1} borderRadius = {8} src = {genre.image_background}/>
-                    <Button fontWeight = {genre.id === selectedGenre?.id ? 'bold' : 'normal' } onClick = {() => onSelectGenre(genre)} variant='link'>{genre.name}</Button>
+                    <Image objectFit='cover' boxSize='32px' paddingY={1} borderRadius = {8} src = {genre.image_background}/>
+                    <Button whiteSpace='normal' textAlign='left' fontWeight = {genre.id === selectedGenre?.id ? 'bold' : 'normal' } onClick = {() => onSelectGenre(genre)} variant='link'>{genre.name}</Button>
                 </HStack>
             </ListItem>
                 
             ))}
         </List>
+        </>
     )
 }
 export default GenreList 
