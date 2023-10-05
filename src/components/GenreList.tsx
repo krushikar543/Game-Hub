f2ff45befb8dd27220c7ad4e6368b88c4b64023a
krushikar543 <krushikar.pennabadi@iiitb.org>
2023-10-05 09:01:16 +0530

    made selected genre bold

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -2,11 +2,12 @@ import FetchGenres from "../hooks/FetchGenres"
 import { HStack, Image, List, ListItem, Spinner, Button } from "@chakra-ui/react";
 import { Genre } from '../hooks/FetchGenres';
 interface Props{
+    selectedGenre : Genre | null;
     onSelectGenre: (genre: Genre) => void;
 }
 
 
-const GenreList = ({onSelectGenre} : Props) => {
+const GenreList = ({selectedGenre, onSelectGenre} : Props) => {
     const {genres, error, isLoading} = FetchGenres();
     if(isLoading) return <Spinner />;
     if(error) return null;
@@ -16,7 +17,7 @@ const GenreList = ({onSelectGenre} : Props) => {
             <ListItem key={genre.id}>
                 <HStack >
                     <Image boxSize='32px' paddingY={1} borderRadius = {8} src = {genre.image_background}/>
-                    <Button onClick = {() => onSelectGenre(genre)} variant='link'>{genre.name}</Button>
+                    <Button fontWeight = {genre.id === selectedGenre?.id ? 'bold' : 'normal' } onClick = {() => onSelectGenre(genre)} variant='link'>{genre.name}</Button>
                 </HStack>
             </ListItem>
                 
